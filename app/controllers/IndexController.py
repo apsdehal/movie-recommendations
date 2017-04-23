3672a9ff4115bf7de09daa6931a52e0f717d5bd2
Amanpreet Singh <apsdehal@gmail.com>
2017-04-23 00:32:00 -0400

    Working frontend

--- app/controllers/IndexController.py
+++ app/controllers/IndexController.py
@@ -1,10 +1,13 @@
 import tornado.web
 import tornado.gen
 import tornado.httpclient
+from config import config
 
 
 class IndexController(tornado.web.RequestHandler):
+    def initialize(self):
+        self.url = "%s:%s/%s" % (config["server_url"], config["server_port"], "search")
 
     @tornado.gen.coroutine
     def get(self):
-        self.render("../views/index.html")
+        self.render("../views/index.html", url=self.url, items=[])
--- app/controllers/SearchController.py
+++ app/controllers/SearchController.py
@@ -1,10 +1,30 @@
 import tornado.web
 import tornado.gen
 import tornado.httpclient
+import json
+
+from config import config
+from app.models.QueryModel import QueryModel
 
 
 class SearchController(tornado.web.RequestHandler):
+    def initialize(self):
+        settings = {
+            "url": config['query_engine_url'],
+            "index": config['index_name'],
+            "type": config['type_name']
+        }
+        self.search_url = "%s:%s/%s" % (config["server_url"], config["server_port"], "search")
+        self.query_model = QueryModel(settings)
 
     @tornado.gen.coroutine
-    def get(self):
-        self.render("app/views/index.html")
+    def post(self):
+        search_type = self.get_body_argument('search_type')
+        query = self.get_body_argument('query')
+        items = self.query_model.searchField(search_type, query)
+        if items is not False:
+            items = json.loads(items)
+            items = items['hits']
+        else:
+            items = {'hits': []}
+        self.render("../views/index.html", url=self.search_url, items=items['hits'])
