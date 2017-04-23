baa3228cce159dc1c1ca1d5b8b7ae22a83989df0
Amanpreet Singh <apsdehal@gmail.com>
2017-04-23 00:31:49 -0400

    Add query model

---
+++ app/models/QueryModel.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+from config import config
+
+
+class QueryModel:
+    def __init__(self, settings):
+        self.url = "%s/%s/%s/_search" % (settings['url'], settings['index'], settings['type'])
+
+    def searchField(self, field, query):
+        payload = {}
+        payload["query"] = {"term": {field: query}}
+        print(self.url, payload)
+        r = requests.get(self.url, data=json.dumps(payload))
+        if r.status_code == 200:
+            return r.content
+        else:
+            return False
