158e0363c7a1dfcb28297792a884bbc4a53c7c54
Amanpreet Singh <apsdehal@gmail.com>
2017-04-27 16:54:02 -0400

    Complete working frontend using React

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import config from "../../../config.json"
+import constants from "./constants";
+import ReactTooltip from "react-tooltip";
 
 class App extends Component {
   constructor() {
@@ -11,12 +12,8 @@ class App extends Component {
           search_type: 'actor_names',
           query: ''
       };
-      console.log(config)
-      this.axiosInstance = axios.create({
-          baseUrl: config["server_url"] + "/" + config["server_port"],
-          responseType: 'json',
 
-      });
+      axios.default.responseType = "json";
   }
 
   handleClick(e) {
@@ -25,8 +22,26 @@ class App extends Component {
       payload["search_type"] = this.searchInput.value;
       payload["query"] = this.queryInput.value;
       let that = this;
-      this.axiosInstance.post("http://localhost:9999/search", JSON.stringify(payload)).then((response) => {
+      axios.post(constants.baseUrl + "/search", JSON.stringify(payload)).then((response) => {
+         let data = response.data;
+         let requests = []
          that.setState({items: response.data});
+
+         for(let i = 0; i < data.length; i++) {
+             let imdbId = data[i]["_source"]["movie_imdb_link"].split("/")[4];
+             let axiosCall = function () {
+                 return axios.get(constants.omdbLink + "?i=" + imdbId);
+             }
+             requests.push(axiosCall());
+         }
+         axios.all(requests).then( (responses) => {
+             for(let i = 0; i < responses.length; i++) {
+                 let response = responses[i]["data"]
+                 data[i]["_source"]["movie_thumbnail"] = response["Poster"];
+                 data[i]["_source"]["movie_plot"] = response["Plot"] || response["Plot"].substr(0, 75);
+             }
+             this.setState({items: data});
+         });
       });
   }
 
@@ -37,40 +52,48 @@ class App extends Component {
         <div className="App-header">
           <h2>Movie Recommendations</h2>
         </div>
-        <div className="App-intro">
-        <div>
-            <label>Select main search type
-                <select name="search_type" defaultValue="actor_names" ref={(input) => that.searchInput = input}>
-                    <option value="actor_names">Actor</option>
-                    <option value="director_name">Director</option>
-                    <option value="movie_title">Movie Name</option>
-                </select>
-            </label>
-            <label>
-                <input name="query" defaultValue="" ref={(input) => that.queryInput = input} />
-            </label>
-            <button onClick={that.handleClick.bind(that)}>Search</button>
-        </div>
-        </div>
-        <div>
-        {
-            that.state.items.length && that.state.items.map((item, i) => {
+        <div className="App-intro container">
+            <div>
+                <label>Select main search type
+                    <select name="search_type" defaultValue="actor_names" ref={(input) => that.searchInput = input}>
+                        <option value="actor_names">Actor</option>
+                        <option value="director_name">Director</option>
+                        <option value="movie_title">Movie Name</option>
+                    </select>
+                </label>
+                <label>
+                    <input name="query" defaultValue="" ref={(input) => that.queryInput = input} />
+                </label>
+                <button onClick={that.handleClick.bind(that)}>Search</button>
+            </div>
+            <div className="row movie-list">
+            {
 
-                item = item["_source"];
-                return (
-                  <div key={i}>
-                      <span><a href={item["movie_imdb_link"]}>{item["movie_title"]}</a></span>
-                      <span>{item["director_name"]}</span>
-                      <span>{item["imdb_score"]}</span>
-                      <span>{item["actor_names"][0]}, {item["actor_names"][1]}</span>
-                  </div>
-                )
+                this.state.items.map((item, index) => {
+                    item = item["_source"]
+                    return (
+                        <div key={index} className="col-lg-3 col-md-4 col-sm-6 col-xs1-8 col-xs-12">
+                           <div className="item">
+                              <a data-tip data-for={"item" + index} className="poster" href={item["movie_imdb_link"]}>
+                                <img src={item["movie_thumbnail"]} alt={item["movie_title"]}/>
+                                <span className="name">{item["movie_title"]}</span>
+                              </a>
+                              <ReactTooltip place="right" effect="solid" className="item-tooltip" id={"item" + index} type="dark">
+                                <div><b>Title:</b> {item["movie_title"]}</div>
+                                <div><b>Actor:</b> {item["actor_names"][0]}, {item["actor_names"][1]}</div>
+                                <div><b>Director:</b> {item["director_name"]}</div>
+                                <div><b>IMDB Score:</b> {item["imdb_score"] + "/10"}</div>
+                                <div><b>Plot:</b> {item["movie_plot"]}</div>
+                              </ReactTooltip>
+                           </div>
+                        </div>
+                    )
+                })
             }
-          )
-        }
-        </div>
+            </div>
 
-      </div>
+         </div>
+       </div>
     );
   }
 }
